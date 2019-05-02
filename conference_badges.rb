dacc8587c2a48f7d85be31763a3555a726ac493f
Jason Fournier <jmf4nier@hotmail.com>
2019-05-02 10:24:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+
+
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.collect do |name|
+     "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(array)
+  array.collect.with_index(1) do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+end
+
+def printer(array)
+  names = array
+  batch_badge_creator(names).each do |name|
+    sleep(1)
+    puts name
+  end
+  assign_rooms(names).each do |rooms|
+    puts rooms
+  end
+end
+
+#printer(names)
+
